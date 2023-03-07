490e066170406892e2d3f19704d54574cb698dd1
netoe-DCTEF <elynetobr@outlook.com>
2023-03-07 20:09:20 -0300

    é o roque

--- giroscopio/App.js
+++ giroscopio/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { Accelerometer } from "expo-sensors";
+import { Gyroscope } from "expo-sensors";
 
 export default function App() {
   const [{ x, y, z }, setData] = useState({
@@ -10,11 +10,15 @@ export default function App() {
   });
   const [subscription, setSubscription] = useState(null);
 
-  const _slow = () => Accelerometer.setUpdateInterval(1000);
-  const _fast = () => Accelerometer.setUpdateInterval(500);
+  const _slow = () => Gyroscope.setUpdateInterval(1000);
+  const _fast = () => Gyroscope.setUpdateInterval(16);
 
   const _subscribe = () => {
-    setSubscription(Accelerometer.addListener(setData));
+    setSubscription(
+      Gyroscope.addListener((gyroscopeData) => {
+        setData(gyroscopeData);
+      })
+    );
   };
 
   const _unsubscribe = () => {
@@ -29,12 +33,10 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Accelerometer: (in gs where 1g = 9.81 m/s^2)
-      </Text>
-      <Text style={styles.text}>x: {x.toFixed(2)}</Text>
-      <Text style={styles.text}>y: {y.toFixed(2)}</Text>
-      <Text style={styles.text}>z: {z.toFixed(2)}</Text>
+      <Text style={styles.text}>Gyroscope:</Text>
+      <Text style={styles.text}>x: {x.toFixed(3)}</Text>
+      <Text style={styles.text}>y: {y.toFixed(3)}</Text>
+      <Text style={styles.text}>z: {z.toFixed(3)}</Text>
       <View style={styles.buttonContainer}>
         <TouchableOpacity
           onPress={subscription ? _unsubscribe : _subscribe}
@@ -42,7 +44,10 @@ export default function App() {
         >
           <Text>{subscription ? "On" : "Off"}</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={_slow} style={[styles.button]}>
+        <TouchableOpacity
+          onPress={_slow}
+          style={[styles.button, styles.middleButton]}
+        >
           <Text>Slow</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={_fast} style={styles.button}>
@@ -57,7 +62,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    paddingHorizontal: 20,
+    paddingHorizontal: 10,
   },
   text: {
     textAlign: "center",
